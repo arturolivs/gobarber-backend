a86f3e131463be587318d24beef7644b806f79e9
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-05-05 19:35:04 -0300

    getAll appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,6 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRoutes = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRoutes.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  response.json(appointments);
+});
+
 appointmentsRoutes.post('/', (request, response) => {
   const { provider, date } = request.body;
 
