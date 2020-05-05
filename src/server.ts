4f17cd6dc462a84c34576c12634b2a664fc6b998
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-05-04 22:05:56 -0300

    config eslint prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-
-app.get("/", (request, response) => {
-  return response.json({ message: "hello world." });
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("server started on port 3333 👍");
+  console.log('server started on port 3333 👍');
 });
