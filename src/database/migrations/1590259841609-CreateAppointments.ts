7c6dd459af075f910c64118aa39e5f695cda23e7
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-05-23 18:37:14 -0300

    Using db to list and add appointments

--- src/database/migrations/1590259841609-CreateAppointments.ts
+++ src/database/migrations/1590259841609-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1590259841609
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
 
           {
