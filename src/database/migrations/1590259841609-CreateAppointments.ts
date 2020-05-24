62ba6bab3645c3d33a0702aec4cb5bab1948a454
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-05-23 23:23:58 -0300

    created appointment user relationship

--- src/database/migrations/1590259841609-CreateAppointments.ts
+++ src/database/migrations/1590259841609-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590259841609
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
