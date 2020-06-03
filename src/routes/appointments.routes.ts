86b5b0136489054b5d5fd6715d14ed46b43247f7
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-06-02 22:07:03 -0300

    exception handling

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -17,21 +17,17 @@ appointmentsRoutes.get('/', async (request, response) => {
 });
 
 appointmentsRoutes.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-    const parseDate = parseISO(date);
+  const { provider_id, date } = request.body;
+  const parseDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      date: parseDate,
-      provider_id,
-    });
+  const appointment = await createAppointment.execute({
+    date: parseDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRoutes;
