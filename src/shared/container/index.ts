b76cf9f5c08d2c9dff017e45b530bccba1ee2475
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-10-27 21:58:27 -0300

    save tokens in database

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,8 +9,8 @@ import IAppointmentRepository from '@modules/appointments/repositories/IAppointm
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-// import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 container.registerSingleton<IAppointmentRepository>(
   'AppointmentsRepository',
@@ -20,4 +20,9 @@ container.registerSingleton<IAppointmentRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokenRepository',
+  UserTokenRepository,
 );
