508701b2d5ad1fb0e5bf2c0d9227262496873b2d
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-11-26 21:00:45 -0300

    send mail with ses

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from '@shared/container/providers/Storage/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/Storage/implementations/DiskStorageProvider';
 
 import IMailProvider from '@shared/container/providers/Mail/models/IMailProvider';
 import EtherealMailProvider from '@shared/container/providers/Mail/implementations/EtherealMailProvider';
+import SESMailProvider from '@shared/container/providers/Mail/implementations/SESMailProvider';
 
 import IMailTemplateProvider from '@shared/container/providers/MailTemplate/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplate/implementations/HandlebarsMailTemplateProvider';
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
