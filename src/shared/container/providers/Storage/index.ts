1c0fc37122d054d685a30d2ad8a5c72af50b5e06
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2020-12-02 22:10:17 -0300

    using S3 to storage in prod

--- src/shared/container/providers/Storage/index.ts
+++ src/shared/container/providers/Storage/index.ts
@@ -1,12 +1,15 @@
 import { container } from 'tsyringe';
 
+import uploadConfig from '@config/upload';
 import IStorageProvider from '@shared/container/providers/Storage/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/Storage/implementations/DiskStorageProvider';
+import S3StorageProvider from '@shared/container/providers/Storage/implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
